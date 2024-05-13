c9fe7263fd6dcff1ca21ac46d9d312d06702fef5
StuartMorais <stuartmorais@hotmail.com>
2024-05-13 14:45:52 -0300

    PrimeiroCommit

---
+++ phpAula/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+  </head>
+  <body>
+
+
+    <?php
+    
+      function somar($v1, $v2, $v3){
+        
+        $soma = $v1 + $v2 + $v3;
+
+        return $soma;
+      }
+
+      echo somar(4, 7, 9);
+
+    ?>
+
+    <?php
+    
+      $nome = "Stuart";
+      echo "<h1>$nome</h1>";
+    
+    ?>
+
+
+
+    <script src="script.js"></script>
+  </body>
+</html>
