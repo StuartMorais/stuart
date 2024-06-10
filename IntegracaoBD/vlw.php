cc956dfadd9f577b2b36c4144a336df51ce976fe
StuartMorais <stuartmorais@hotmail.com>
2024-06-10 17:13:05 -0300

    integração com o banco

--- IntegracaoBD/vlw.php
+++ IntegracaoBD/vlw.php
@@ -1,17 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
 
-    <body>
-        <?php
-        
-            require('server.php');
-        
-        ?>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php require('server.php'); ?>
+
+    <?php
+
+        $sql = $pdo->prepare("SELECT * FROM livro;");
+        $sql->execute();
+
+        $dados = $sql->fetchAll();
+
+    ?>
+
+    <table class="table table-hover table-dark table-striped">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Titulo</th>
+                <th scope="col">Ano</th>
+                <th scope="col">Paginas</th>
+                <th scope="col">Autor</th>
+            </tr>
+        </thead>
+        <tbody>
+
+                <?php
+                
+                    foreach($dados as $chave => $valor){
+
+                        echo "
+                            <tr>
+                            
+                                <th scope='row'>".$valor['id_livro']."</th>
+                                <td>".$valor['titulo']."</td>
+                                <td>".$valor['ano_cri']."</td>
+                                <td>".$valor['qtd_paginas']."</td>
+                                <td>".$valor['autor']."</td>
+                            
+                            </tr>";
+
+                    }
+                
+                ?>
+                
+        </tbody>
+    </table>
+
+</body>
 
 </html>
